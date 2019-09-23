fcbfb7049e588fc18e632bdf42f28a4aa71870be
1632347 <1632347@cstjean.qc.ca>
2019-09-23 09:38:18 -0400

    First

---
+++ Assets/ThirdPersonCamera.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+public class ThirdPersonCamera : MonoBehaviour
+{
+    public float Sensibilité = 10;
+    public Transform cible;
+    public float DistanceCible = 2;
+    public Vector2 MaxAxeY = new Vector2(-40, 85);
+
+    public float DelaiRotation = 0.3f;
+    Vector3 VelociteRotation;
+    Vector3 RotationActuel;
+
+    float AxeX;
+    float AxeY;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        AxeX += Input.GetAxis("Mouse X");
+        AxeY -= Input.GetAxis("Mouse Y");
+        AxeY = Mathf.Clamp(AxeY, MaxAxeY.x, MaxAxeY.y);
+
+        RotationActuel = Vector3.SmoothDamp(RotationActuel, new Vector3(AxeY, AxeX), ref VelociteRotation, DelaiRotation);
+
+        //angle sur 360 degrès
+        transform.eulerAngles = RotationActuel;
+
+        transform.position = cible.position - transform.forward * DistanceCible;
+
+
+    }
+}
---
+++ Assets/boug.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boug : MonoBehaviour
+{
+
+    public float vitesseMarche = 2;
+    public float vitesseCourse = 6;
+
+    public float tamponTournee = 0.2f;
+    private float velociteTourner;
+
+    public float tamponVitesse = 0.1f;
+    private float velociteVitesse;
+    private float vitesseActuel;
+
+    Animator animateur;
+    Transform transformCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animateur = GetComponent<Animator>();
+        transformCamera = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 inputDir = input.normalized;
+
+        if (inputDir != Vector2.zero)
+        {
+            float rotationVisee = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + transformCamera.eulerAngles.y;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, rotationVisee, ref velociteTourner, tamponTournee);
+
+        }
+
+        bool estCourse = Input.GetKey(KeyCode.LeftShift);
+        float vitesseVisee = ((estCourse) ? vitesseCourse : vitesseMarche) * inputDir.magnitude;
+        vitesseActuel = Mathf.SmoothDamp(vitesseActuel, vitesseVisee, ref velociteVitesse, tamponVitesse);
+
+
+        transform.Translate(transform.forward * vitesseActuel * Time.deltaTime, Space.World);
+
+        float vitesseAnimation = ((estCourse) ? 1 : 0.65f) * inputDir.magnitude;
+        animateur.SetFloat("pourcentageVitesse", vitesseAnimation, tamponVitesse, Time.deltaTime);
+    }
+}
+
